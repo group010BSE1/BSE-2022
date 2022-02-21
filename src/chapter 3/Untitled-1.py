854a4b3196eefea579ce953a00d0c94a37605e28
group010BSE1 <arindaandrew11@gmail.com>
2022-02-21 22:28:16 +0300

    Assignment 3

--- src/chapter 3/Untitled-1.py
+++
@@ -1,9 +0,0 @@
-def compute_pay(h,r):
-    if h >40:
-        xh= h -40
-        pay=(1.5*r*xh)+(40*r)
-        print(pay)
-    else : 
-        pay = h*r
-        print(pay)
-print(compute_pay(45,10))
