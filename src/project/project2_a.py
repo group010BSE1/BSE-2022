70697050b1da630611f7d5b357a460ccb8d9b71a
group010BSE1 <arindaandrew11@gmail.com>
2022-03-14 14:57:36 +0300

    project

---
+++ src/project/project2_a.py
@@ -0,0 +1,22 @@
+try:
+    file_name = open("project/measles.txt")   
+except:
+    print("Un-able to open the file")
+    exit
+
+file_name = open("project/measles.txt") 
+output_file = input("Enter the output file : ")
+year = input("Enter the year : ")
+with open(output_file, "w") as file:
+    for line in file_name:
+        if year.lower() == "all" or year == "": #All, all, "Empty Input"
+            file.write(line)
+        elif int(year) == int(line[88:]):# year
+            file.writelines(line)
+        elif int(year) == int(line[88]):
+            file.writelines(line)
+        elif int(year) == int(line[88:90]):
+            file.writelines(line)
+        elif int(year) == int(line[88:91]):
+            file.writelines(line)
+        
